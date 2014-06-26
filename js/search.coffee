ef80770f6ec067d87097bf3baf287113750acfb1
Joe Teo <revelate@live.com>
2014-06-26 17:32:19 +0800

    add directives and scopes

--- js/search.coffee
+++ js/search.coffee
@@ -1,4 +1,27 @@
 omdbSearcher = angular.module 'omdbSearcher', []
 
-omdbSearcher.controller 'searchInput',  ['$scope'
-  ]
+omdbSearcher.controller 'MovieController', ['$scope', '$http', ($scope, $http)->
+  $scope.movieList = []
+
+  $scope.search = (movie) ->
+    $http(
+      method: "GET",
+      url: "http://www.omdbapi.com/",
+      params: {s: movie}
+    ).success( (data, status, headers, config) ->
+      $scope.movieList = data.Search
+      # console.log "it's finally working!"
+    ).error (data, status, headers, config) ->
+      console.log "unable to find movie"
+
+  $scope.showDetails = (movie) ->
+    $http(
+      method: "GET",
+      url: "http://www.omdbapi.com/",
+      params: {i: movie.imdbID}
+    ).success( (data, status, headers, config) ->
+      $scope.movieDetails = data
+      console.log $scope.movieDetails
+    ).error (data, status, headers, config) ->
+      console.log "unable to find info"
+]
--- js/search.js
+++ js/search.js
@@ -4,4 +4,37 @@
 
   omdbSearcher = angular.module('omdbSearcher', []);
 
+  omdbSearcher.controller('MovieController', [
+    '$scope', '$http', function($scope, $http) {
+      $scope.movieList = [];
+      $scope.search = function(movie) {
+        return $http({
+          method: "GET",
+          url: "http://www.omdbapi.com/",
+          params: {
+            s: movie
+          }
+        }).success(function(data, status, headers, config) {
+          return $scope.movieList = data.Search;
+        }).error(function(data, status, headers, config) {
+          return console.log("unable to find movie");
+        });
+      };
+      return $scope.showDetails = function(movie) {
+        return $http({
+          method: "GET",
+          url: "http://www.omdbapi.com/",
+          params: {
+            i: movie.imdbID
+          }
+        }).success(function(data, status, headers, config) {
+          $scope.movieDetails = data;
+          return console.log($scope.movieDetails);
+        }).error(function(data, status, headers, config) {
+          return console.log("unable to find info");
+        });
+      };
+    }
+  ]);
+
 }).call(this);
